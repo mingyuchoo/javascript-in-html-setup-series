9eab0c1859c2ca2926b3de3e80a7918684bb04ec
mingyuchoo <mingyuchoo@gmail.com>
2019-08-05 17:39:50 +0900

    added

---
+++ jest/11-repeating-setup-for-many-tests/README.md
@@ -0,0 +1,11 @@
+# jest 01
+
+references
+ - https://jestjs.io/docs/en/setup-teardown
+
+## Configuration
+ - jest --init
+
+
+## Commands
+ - npm run test
---
+++ jest/11-repeating-setup-for-many-tests/src/repeating-setup.test.js
@@ -0,0 +1,16 @@
+beforeEach(() => {
+  initializeCityDatabase();
+});
+
+afterEach(() => {
+  clearCityDatabase();
+});
+
+
+test('city database has Vienna', () => {
+  expect(isCity('Vienna')).toBeTruthy();
+});
+
+test('city database has San Juan', () => {
+  expect(isCity('San Juan')).toBeTruthy();
+});
