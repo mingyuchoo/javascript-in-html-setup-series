84d0f3134eea23fe1754a63842507b14e600c156
mingyuchoo <mingyuchoo@gmaiil.com>
2020-05-13 06:32:27 +0900

    파일 정리

--- basics/00-template/src/index.js
+++ basics/00-template/src/index.js
@@ -1,9 +1,9 @@
 const sum = (a, b) => {
   return a + b;
-}
+};
 
 const hello = (name) => {
-  document.getElementById("output").innerHTML = "Hello, " + name;
-}
+  document.getElementById('output').innerHTML = 'Hello, ' + name;
+};
 
-export default sum;
+//export default sum;
