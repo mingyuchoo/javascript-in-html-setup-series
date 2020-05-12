2747780086bab92230f0d2697dbb4f69ec58c65a
mingyuchoo <mingyuchoo@gmaiil.com>
2020-05-13 07:02:48 +0900

    modified

---
+++ basics/00-template/public/javascripts/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var sum = function sum(a, b) {
+  return a + b;
+};
+
+var hello = function hello(name) {
+  document.getElementById('output').innerHTML = 'Hello, ' + name;
+}; //export default sum;
---
+++ basics/00-template/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public/javascripts'),
+        publicPath: '/public/javascripts/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname),
+                exclude: /(node_modules)|(public)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
