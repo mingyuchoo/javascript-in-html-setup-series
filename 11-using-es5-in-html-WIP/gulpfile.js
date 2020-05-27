0fcfb07fef109a0629cbc4f88a6c9542d1b48eb8
mingyuchoo <mingyuchoo@gmail.com>
2020-05-28 06:12:54 +0900

    modified

--- 11-using-es5-in-html-WIP/gulpfile.js
+++ 11-using-es5-in-html-WIP/gulpfile.js
@@ -1,52 +1,31 @@
 // https://gulpjs.com/docs/en/getting-started/creating-tasks
 
-const { series, parallel } = require('gulp');
+const { series, src, dest, task, watch } = require('gulp');
+const clean = require('gulp-clean');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
-function clean(cb) {
-  // body omitted
-  cb();
-}
+task('taskClean', function () {
+  return src('public/js/*')
+    .pipe(clean({ force: true }))
+    .pipe(src('public/css/*'))
+    .pipe(clean({ force: true }));
+});
 
-function cssTranspile(cb) {
-  // body omitted
-  cb();
-}
+task('taskSASS', function () {
+  return src('src/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(dest('public/css'));
+});
 
-function cssMinify(cb) {
-  // body omitted
-  cb();
-}
+task('taskJavaScript', function () {
+  return src('src/js/index.js')
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(dest('public/js'));
+});
 
-function jsTranspile(cb) {
-  // body omitted
-  cb();
-}
-
-function jsBundle(cb) {
-  // body omitted
-  cb();
-}
-
-function jsMinify(cb) {
-  // body omitted
-  cb();
-}
-
-function publish(cb) {
-  // body omitted
-  cb();
-}
-
-exports.build = series(
-  clean,
-  parallel(cssTranspile, series(jsTranspile, jsBundle)),
-  parallel(cssMinify, jsMinify),
-  publish
-);
-
-exports.default = series(
-  clean,
-  parallel(cssTranspile, series(jsTranspile, jsBundle)),
-  parallel(cssMinify, jsMinify),
-  publish
-);
+task('default', series('taskClean', 'taskSASS', 'taskJavaScript'));
